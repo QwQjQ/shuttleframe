329a23cbe6ecd37550694eb3cf026078dde175a2
julialoxer <l19180488673@163.com>
2022-05-18 18:34:48 +0800

    初始化项目

---
+++ src/components/shuttle-frame.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="leftFrame">
+      <div class="SearchBox">
+        <input type="text" placeholder="请输入" />
+      </div>
+
+      <div v-for="(item,index) in leftList" :key="index">
+        <div class="leftListItem">
+          <input type="checkbox" value="item" />
+          <span>{{item}}</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="rightFrame">
+      <div class="SearchBox">
+        <input type="text" placeholder="请输入" />
+      </div>
+      <div v-for="(item,index) in rightList" :key="index">
+        <div class="rightListItem">
+          <p>
+            <input type="checkbox" value="item" />
+            {{item}}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "shuttle-frame",
+
+  props: {
+    leftList: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    rightList: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      // 左边参数列表
+      //   leftList: [],
+      // 右边参数列表
+      //   rightList: []
+    };
+  },
+
+  mounted() {
+    console.log(this.leftList);
+    console.log(this.rightList);
+  },
+
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <shuttleFrame :leftList="leftList" :rightList="rightList"></shuttleFrame>
+  </div>
+</template>
+
+<script>
+import shuttleFrame from "../components/shuttle-frame.vue";
+
+export default {
+  name: "WorkspaceJsonIndex",
+  components: {
+    shuttleFrame
+  },
+  data() {
+    return {
+      leftList: ["111", "222", "333", "444", "555"],
+      rightList: ["111", "222", "333", "444", "555"]
+    };
+  },
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
